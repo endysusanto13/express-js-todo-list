b0dccb21fe1d0fe7a20ad50abf9dd9a1339a713d
endysusanto13 <endy.susanto@gmail.com>
2021-10-17 13:18:45 +0800

    Add test for connecting to database

---
+++ src/db/index.test.js
@@ -0,0 +1,14 @@
+require('dotenv').config({ path: '.env.test' })
+const db = require('./index')
+
+describe('Creating Tables', () => {
+  it('should not throw error', async () => {
+    await db.deleteAllTables()
+    expect(() => db.initialise()).not.toThrow()
+  })
+})
+
+afterAll(async () => {
+  await db.end()
+})
+
